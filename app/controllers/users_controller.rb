b62764b7d4478df3af2fd0e72478c1a61d1c0d84
EC2 Default User <ec2-user@ip-172-31-19-235.ec2.internal>
2022-03-28 05:32:53 +0000

    [Add]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -44,5 +46,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:create, :index, :show, :edit, :update]
-  get '/homes/about' => 'homes#about',as:'about'
+  get '/home/about' => 'homes#about',as:'about'
 end
